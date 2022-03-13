2576a31937a20ab88bb446dda91f4585e41219db
BorisMilanov <boris.mil.96@gmail.com>
2022-03-13 17:56:09 +0200

    Add css and constants.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const { PORT } = require('./constants');
+
 const app = express();
 
-const port = 5000;
-require('./config/hbsConfig')(app);
+require('./config/express-config')(app);
+require('./config/hbs-config')(app);
 
 app.get('/', (req, res) => {
-    res.render('test',{layout:false});
+    res.render('home');
 });
 
-app.listen(port, () => {
-    console.log(`The app is running on http://localhost:${port}/`)
+app.listen(PORT, () => {
+    console.log(`The app is running on http://localhost:${PORT}/`)
 })
