d998fb3c3e2d8c27478931614a37b0dcff58a29e
BorisMilanov <boris.mil.96@gmail.com>
2022-03-13 18:26:32 +0200

    Add home route.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const { PORT } = require('./constants');
 
 const app = express();
 
+const routes = require('./routes');
+
 require('./config/express-config')(app);
 require('./config/hbs-config')(app);
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+
+app.use(routes);
+
 
 app.listen(PORT, () => {
     console.log(`The app is running on http://localhost:${PORT}/`)
-})
+});
