5f12758da064e74994515f13217102a9efe505fc
BorisMilanov <boris.mil.96@gmail.com>
2022-03-14 14:04:13 +0200

    Setup routes register and login.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-const homeController = require('./controllers/home-controller')
+const homeController = require('./controllers/home-controller');
+const authController = require('./controllers/auth-conttroler');
 
 router.use(homeController);
+router.use('/auth', authController);
 
 module.exports = router;
