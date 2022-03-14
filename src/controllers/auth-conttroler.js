86d250cdeaa9b802f0ab858f7096177ca2c1fbbb
BorisMilanov <boris.mil.96@gmail.com>
2022-03-14 19:47:44 +0200

    Add user registration.

--- src/controllers/auth-conttroler.js
+++ src/controllers/auth-conttroler.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const {register} = require('../services/authService');
 
 router.get('/login', (req, res) => {
     res.render('auth/login')
 });
 
-router.get('/register', (req, res) =>{
+router.get('/register', (req, res) => {
     res.render('auth/register');
-})
+});
+
+router.post('/register', async (req, res) => {
+    let {name, username, password, rePassword} = req.body;
+    if(password !== rePassword){
+        res.locals.error = 'Password missmatch';
+        return res.render('auth/register');
+    }
+
+    await register({name, username, password,});
+    res.redirect('/');
+});
 
 module.exports = router;
