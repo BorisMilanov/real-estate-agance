32966a1a55c542e8eb298a150a6a6134943f1c17
BorisMilanov <boris.mil.96@gmail.com>
2022-03-26 16:13:39 +0200

    Housing controller and service added.

---
+++ src/controllers/housing-controller.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const housingService = require('../services/housing-service');
+
+router.get('/aprt-for-recent', (req, res) => {
+    res.render('housing/aprt-for-recent')
+});
+
+router.get('/create', (req, res) => {
+    res.render('housing/create')
+});
+
+router.post('/create', async ( req, res) => {
+    await housingService.create(req.body);
+
+    res.redirect('/')
+});
+
+module.exports = router;
